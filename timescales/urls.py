0c513b6a72a6ec76d0bfa6eda27dfb188a116349
Tanveer Najib <tanveernajib@gmail.com>
2022-02-27 17:37:59 +0000

    added skeleton detail pages for timescales

--- timescales/urls.py
+++ timescales/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.timescales, name='timescales')
+    path('', views.timescales, name='timescales'),
+    path('eons/<int:eon_id>/', views.eon_detail, name='eon'),
+    path('eras/<int:era_id>/', views.era_detail, name='era'),
+    path('periods/<int:period_id>/', views.period_detail, name='period')
 ]
--- timescales/views.py
+++ timescales/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . import models
 
 # Create your views here.
@@ -13,3 +13,24 @@ def timescales(request):
         'geo_periods': geoperiods
     }
     return render(request, 'timescales/index.html', context=context)
+
+def eon_detail(request, eon_id):
+    eon = get_object_or_404(models.GeoEon, id=eon_id)
+    context = {
+        'eon': eon
+    }
+    return render(request, 'timescales/eon.html', context=context)
+
+def era_detail(request, era_id):
+    era = get_object_or_404(models.GeoEra, id=era_id)
+    context = {
+        'era': era
+    }
+    return render(request, 'timescales/era.html', context=context)
+
+def period_detail(request, period_id):
+    period = get_object_or_404(models.GeoPeriod, id=period_id)
+    context = {
+        'period': period
+    }
+    return render(request, 'timescales/period.html', context=context)
