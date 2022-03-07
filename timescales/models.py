dbe4f283fc2f86598100257366cecc78b70fb848
Tanveer Najib <tanveernajib@gmail.com>
2022-03-07 13:31:36 +0000

    made absolute number unique

--- timescales/models.py
+++ timescales/models.py
@@ -5,7 +5,7 @@ class GeoPeriod(models.Model):
     name = models.CharField(max_length=50)
     extent_mya = models.IntegerField(default=0)
     duration_moy = models.IntegerField(default=0)
-    absolute_number = models.IntegerField(default=0)
+    absolute_number = models.IntegerField(default=0, unique=True)
     period_order_number = models.IntegerField(default=0)
     summary = models.TextField()
     life = models.TextField(blank=True, null=True)
@@ -16,7 +16,7 @@ class GeoPeriod(models.Model):
 class GeoEra(models.Model):
     name = models.CharField(max_length=50)
     periods = models.ManyToManyField(GeoPeriod, blank=True)
-    absolute_number = models.IntegerField(default=0)
+    absolute_number = models.IntegerField(default=0, unique=True)
     era_order_number = models.IntegerField(default=0)
     summary = models.TextField()
     life = models.TextField(blank=True, null=True)
@@ -27,7 +27,7 @@ class GeoEra(models.Model):
 class GeoEon(models.Model):
     name = models.CharField(max_length=50)
     eras = models.ManyToManyField(GeoEra, blank=True)
-    absolute_number = models.IntegerField(default=0)
+    absolute_number = models.IntegerField(default=0, unique=True)
     summary = models.TextField()
     life = models.TextField(blank=True, null=True)
 
